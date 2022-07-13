545471e37076bcc1308cefe8cd1eda096f94c918
Olga <olga@gmail.com>
2022-07-13 20:55:17 +0300

    refactor(hw-36): replace div with ul

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -6,13 +6,13 @@ const Slider = () => {
             <h1 className="slider__title">The Power of Simplicity</h1>
             <p className="slider__description">Instead of spending time searching for the right app, our AI will bring the right app to you.</p>
             <a className="slider__button" href="#">Learn</a>
-            <div className="slider__dots">
-                <button className="slider__dot"></button>
-                <button className="slider__dot"></button>
-                <button className="slider__dot slider__dot--active"></button>
-                <button className="slider__dot"></button>
-                <button className="slider__dot"></button>
-            </div>
+            <ul className="slider__dots">
+                <li className="slider__dot"></li>
+                <li className="slider__dot"></li>
+                <li className="slider__dot slider__dot--active"></li>
+                <li className="slider__dot"></li>
+                <li className="slider__dot"></li>
+            </ul>
         </div>
     );
 }

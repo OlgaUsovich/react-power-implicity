4153535da7e558617e834c3e4e6bb2f064a613ea
Olga <olga@gmail.com>
2022-07-13 20:49:39 +0300

    refactor(hw-36): move svg image to file

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,13 @@
 import "./index.scss";
 import { ReactComponent as Logo } from "../../assets/logo.svg"
+import { ReactComponent as Burger } from "../../assets/burger-menu.svg"
 
 const Navbar = () => {
     return (
         <header className="navbar">
             <Logo />
             <button className="navbar__burger">
-            <svg width="55" height="56" viewBox="0 0 55 56" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M14 19H38V22H14V19ZM14 26.5H38V29.5H14V26.5ZM14 34H38V37H14V34Z" fill="white" />
-            </svg>
+            <Burger />
             </button>
             <ul className="navbar__links">
                 <li><a className="navbar__link" href="#">Partners</a></li>
